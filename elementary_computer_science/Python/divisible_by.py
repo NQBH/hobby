c95bb186157d219db6fb948081f398e69e756dfd
NQBH <nguyenquanbahong@gmail.com>
2022-11-18 20:07:51 +0700

    Python program: check if an integer is divisible by another integer or not

---
+++ elementary_computer_science/Python/divisible_by.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a number a = "))
+b = int(input("Enter a number b = "))
+if (a % b) == 0:
+   print("{0} is divisible by {0}".format(a).format(b))
+else:
+   print("{0} is not divisible by {0}".format(a).format(b))
