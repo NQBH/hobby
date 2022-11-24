45453d554ee05dc122065ecd3acc4915d22a3bbf
NQBH <nguyenquanbahong@gmail.com>
2022-11-24 13:59:06 +0700

    added acute & obtuse triangle

--- elementary_computer_science/Python/triangle.py
+++ elementary_computer_science/Python/triangle.py
@@ -3,11 +3,13 @@ b = float(input("Enter a number b = "))
 c = float(input("Enter a number c = "))
 
 if (a > 0) and (b > 0) and (c > 0) and (a + b > c) and (b + c > a) and (c + a > b):
-	print("{0},{0},{0} are 3 edges of a triangle".format(a).format(b).format(c))
+	print("{},{},{} are 3 edges of a triangle.".format(a,b,c))
 	if (a**2 == b**2 + c**2) or (b**2 == c**2 + a**2) or (c**2 == a**2 + b**2):
-		print("{0},{0},{0} are 3 edges of a right triangle".format(a).format(b).format(c))
+		print("{},{},{} are 3 edges of a right triangle.".format(a,b,c))
 	else:
-		print("{0},{0},{0} are not 3 edges of a right triangle".format(a).format(b).format(c))
+		if (a**2 > b**2 + c**2) or (b**2 > c**2 + a**2) or (c**2 > a**2 + b**2):
+			print("{},{},{} are 3 edges of an obtuse triangle.".format(a,b,c))
+		else:
+			print("{},{},{} are 3 edges of an acute triangle.".format(a,b,c))
 else:
-	print("{0},{0},{0} are not 3 edges of a triangle".format(a).format(b).format(c))
-
+	print("{},{},{} are not 3 edges of a triangle.".format(a,b,c))
