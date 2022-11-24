8aaddf6cc7a8e5aa08969491183fa77741c7a6dd
NQBH <nguyenquanbahong@gmail.com>
2022-11-24 13:46:22 +0700

    added 3 edges of a right triangle

--- elementary_computer_science/Python/triangle.py
+++ elementary_computer_science/Python/triangle.py
@@ -4,5 +4,10 @@ c = float(input("Enter a number c = "))
 
 if (a > 0) and (b > 0) and (c > 0) and (a + b > c) and (b + c > a) and (c + a > b):
 	print("{0},{0},{0} are 3 edges of a triangle".format(a).format(b).format(c))
+	if (a**2 == b**2 + c**2) or (b**2 == c**2 + a**2) or (c**2 == a**2 + b**2):
+		print("{0},{0},{0} are 3 edges of a right triangle".format(a).format(b).format(c))
+	else:
+		print("{0},{0},{0} are not 3 edges of a right triangle".format(a).format(b).format(c))
 else:
 	print("{0},{0},{0} are not 3 edges of a triangle".format(a).format(b).format(c))
+
