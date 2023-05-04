522f470f665058ff58c235d644c44e84c012e19f
NQBH <nguyenquanbahong@gmail.com>
2023-05-05 00:31:53 +0700

    compute sqrt

--- elementary_computer_science/Pascal/square_root.pas
+++ elementary_computer_science/Pascal/square_root.pas
@@ -1,9 +1,9 @@
 program square_root;
-var a, sqrt_a: real;
+var num, sqrt_num: real;
 
 begin
-	write('Enter a number a = ');
-	readln(a);
-	sqrt_a := Sqrt(a);
-	writeln('sqrt(a) = ', sqrt_a)
+	write('Enter a number num = ');
+	readln(num);
+	sqrt_num := Sqrt(num);
+	writeln('sqrt of ', num,' = ', sqrt_num)
 end.
