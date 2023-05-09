e50e0621b26d42f58a44bcaaacd28b5e9407afc8
NQBH <nguyenquanbahong@gmail.com>
2023-05-10 00:50:47 +0700

    Python script: strong number

---
+++ elementary_computer_science/Python/strong_number.py
@@ -0,0 +1,21 @@
+from math import *
+# Function: Check prime 
+def is_prime(x):
+	if x == 1: return 0
+	for i in range (2, int(sqrt(x)) + 1):
+		if x % i == 0: return 0
+	return 1
+
+s = ""
+n = 1
+while n <= 1000:
+	flag = 0
+	for i in range(2, n):
+		if is_prime(i) and n % i == 0:
+			if n % (i*i) != 0:
+				flag = 0
+				break
+			else: flag = 1
+	if flag == 1: s += str(n) + " "
+	n += 1
+print(s)
