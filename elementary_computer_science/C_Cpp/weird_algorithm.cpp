16519a92374dfafc7e8278f03a24b27f3f9dcce9
NQBH <nguyenquanbahong@gmail.com>
2023-05-15 13:03:29 +0700

    C++: weird algorithm

---
+++ elementary_computer_science/C_Cpp/weird_algorithm.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	while (1) {
+		cout << n << " ";
+		if (n == 1) break;
+		if (n%2 == 0) n /= 2;
+		else n = n*3 + 1;
+	}
+	cout << "\n";
+}
