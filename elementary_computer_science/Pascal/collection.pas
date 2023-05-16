240b8fb69bb060aba44128a28f4efbeebfdf0acb
NQBH <nguyenquanbahong@gmail.com>
2023-05-17 01:26:12 +0700

    Pascal: list_prime & Eratosthene

--- elementary_computer_science/Pascal/collection.pas
+++ elementary_computer_science/Pascal/collection.pas
@@ -25,9 +25,39 @@ begin
 	exit(k > sqrt_n);
 end;
 
+procedure list_prime(n: longint);
+var m: longint;
+begin
+	for m := 2 to n do
+		if is_prime(m) then writeln(m);
+end;
+
+procedure Eratosthene(n: longint);
+const MAX = 1000000;
+var i,j: longint;
+	prime: array [1..MAX] of byte;
+begin
+	fillchar(prime, sizeof(prime), 0);
+	for i := 2 to trunc(sqrt(n)) do
+		if prime[i] = 0 then
+		begin
+			j := i*i;
+			while j <= n do
+			begin
+				prime[j] := 1;
+				j := j + i;
+			end;
+		end;
+	for i := 2 to n do
+		if prime[i] = 0 then writeln(i);
+end;
+
 begin
 	write('Enter the value of integer n = ');
 	readln(n);
-	write(is_prime(n),' ', is_prime_modified(n));
+	writeln(is_prime(n),' ', is_prime_modified(n));
+	list_prime(n);
+	writeln;
+	Eratosthene(n);
 	writeln
 end.
