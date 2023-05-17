cbbd2365ce3f47de8b91c3f81e416f27b5159292
NQBH <nguyenquanbahong@gmail.com>
2023-05-17 10:29:24 +0700

    Pascal: Fibonacci sequence

--- elementary_computer_science/Pascal/collection.pas
+++ elementary_computer_science/Pascal/collection.pas
@@ -1,7 +1,7 @@
 program collection;
 uses crt;
 var a,b,n: integer;
-
+// Func: check prime
 function is_prime(n: longint): boolean;
 var k: longint;
 begin
@@ -10,7 +10,7 @@ begin
 		if (n mod k = 0) then exit(false);
 	exit(true);
 end;
-
+// Func: modified check prime
 function is_prime_modified(n: longint): boolean;
 var k, sqrt_n: longint;
 begin
@@ -24,14 +24,14 @@ begin
 	until k > sqrt_n;
 	exit(k > sqrt_n);
 end;
-
+// Func: list all primes in an interval
 procedure list_prime(n: longint);
 var m: longint;
 begin
 	for m := 2 to n do
 		if is_prime(m) then writeln(m);
 end;
-
+// Func: list all primes in an interval by Eratosthene's sieve
 procedure Eratosthene(n: longint);
 const MAX = 1000000;
 var i,j: longint;
@@ -51,7 +51,7 @@ begin
 	for i := 2 to n do
 		if prime[i] = 0 then writeln(i);
 end;
-
+// Func: Compute gcd
 function gcd(a,b: longint): longint;
 var tmp: longint;
 begin
@@ -62,10 +62,24 @@ begin
 	end;
 	exit(a)
 end;
-
+// Func: Compute lcm
 function lcm(a,b: longint): longint;
 begin
 	exit(round(a*b/gcd(a,b)))
+end;
+// Func: Fibonacci sequence
+function Fibonacci(n: longint): longint;
+var  i,fi,fi1,fi2: longint;
+begin
+	if n <= 1 then exit(n);
+	fi2 := 0; fi1 := 1;
+	for i := 2 to n do
+	begin
+		fi := fi1 + fi2;
+		fi2 := fi1;
+		fi1 := fi;
+	end;
+	exit(fi);
 end;
 
 begin
@@ -77,5 +91,6 @@ begin
 	writeln;
 	write('Enter the value of integer a = '); readln(a);
 	write('Enter the value of integer b = '); readln(b);
-	writeln('gcd(',a,',',b,') = ',gcd(a,b), ', lcm(',a,',',b,') = ', lcm(a,b))
+	writeln('gcd(',a,',',b,') = ',gcd(a,b), ', lcm(',a,',',b,') = ', lcm(a,b));
+	writeln('F_',n,' = ',Fibonacci(n))
 end.
