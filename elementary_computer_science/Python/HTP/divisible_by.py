19be15e198b160642b432a37d7a2ec1c91b383f5
NQBH <nguyenquanbahong@gmail.com>
2023-05-28 13:54:40 +0700

    HTP's Python: divisible by

---
+++ elementary_computer_science/Python/HTP/divisible_by.py
@@ -0,0 +1,9 @@
+try:
+	a = int(input('nhap a='))
+	b = int(input('nhap b='))
+	if a%b == 0 :
+		print(str(a)+' chia het cho	'+str(b))
+	else:
+		print(str(a)+' khong chia het cho '+str(b))
+except:
+	print('Day khong phai la 1 so nguyen')
