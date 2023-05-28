3d3fef4f1963fec166ca31551aaa64dc1189d0bd
NQBH <nguyenquanbahong@gmail.com>
2023-05-28 14:11:12 +0700

    formatted

--- elementary_computer_science/Python/HTP/divisible_by.py
+++ elementary_computer_science/Python/HTP/divisible_by.py
@@ -1,9 +1,9 @@
 try:
-	a = int(input('nhap a='))
-	b = int(input('nhap b='))
+	a = int(input('Nhap a = '))
+	b = int(input('Nhap b = '))
 	if a%b == 0 :
-		print(str(a)+' chia het cho	'+str(b))
+		print(str(a) + ' chia het cho ' + str(b))
 	else:
-		print(str(a)+' khong chia het cho '+str(b))
+		print(str(a) + ' khong chia het cho ' + str(b))
 except:
-	print('Day khong phai la 1 so nguyen')
+	print('Day khong phai la 1 so nguyen.')
