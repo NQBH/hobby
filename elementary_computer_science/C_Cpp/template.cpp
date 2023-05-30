69ac1866e77a54437e6028d5d4e6e4fa5d568651
NQBH <nguyenquanbahong@gmail.com>
2023-05-30 23:45:02 +0700

    C++ template for CP

---
+++ elementary_computer_science/C_Cpp/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	// Input & Output
+	freopen("prob.inp", "r", stdin);
+	freopen("prob.out", "w", stdout);
+	// 
+
+}
+// Compile in Terminal:
+// g++ -std=c++11 -O2 -Wall test.cpp -o test
