44f20860df9b2752e4e0fa1ec115dad26fe52687
NQBH <nguyenquanbahong@gmail.com>
2023-05-31 00:14:59 +0700

    C++: test long double by computing sqrts

---
+++ elementary_computer_science/C_Cpp/sqrt.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	long double x = sqrt(2);
+	long double y = sqrt(3);
+	printf("%.60Lf, %.60Lf\n", x, y);
+}
