505c19262d762c282fce212f175370e4f0e6218d
NQBH <nguyenquanbahong@gmail.com>
2023-06-02 15:14:57 +0700

    Python CSES: missing number with 4x faster input

---
+++ elementary_computer_science/Python/missing_number_io.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+A = [int(i) for i in A]
+A.sort()
+if A[-1] == n - 1:
+	print(n)
+else:
+	for i in range(n-1):
+		if A[i] != (i + 1):
+			print(i + 1)
+			break
+# Terminal:
+# python3 missing_number_io.py < missing_number.inp > missing_number.out
+# python3 missing_number_io.py < missing_number.inp | tee missing_number.out
