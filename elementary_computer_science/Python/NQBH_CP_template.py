6f0fb53d84ea443f6add27de5e46777be5f47c2d
NQBH <nguyenquanbahong@gmail.com>
2023-06-02 21:12:43 +0700

    added another method for inp out files

--- elementary_computer_science/Python/NQBH_CP_template.py
+++ elementary_computer_science/Python/NQBH_CP_template.py
@@ -1,3 +1,8 @@
+"""
+import sys
+sys.stdin = open("prob.inp", "r")
+sys.stdout = open("prob.out", "w")
+"""
 file_in = open("prob.inp")
 file_out = open("prob.out", "w")
 n = int(file_in.readline())
