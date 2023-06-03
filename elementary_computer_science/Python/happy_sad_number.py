3aef159e8b99729c2c221331b0a5d5039aa3faf4
NQBH <nguyenquanbahong@gmail.com>
2023-06-03 21:05:39 +0700

    Python: happy & sad numbers

---
+++ elementary_computer_science/Python/happy_sad_number.py
@@ -0,0 +1,17 @@
+def sum_digit_sqr(n):
+	sum = 0 
+	for i in str(n):
+		sum += int(i)**2
+	return sum
+n0 = n = int(input())
+count = 0
+while n > 1 and count <= 1000:
+	n = sum_digit_sqr(n)
+	print(n)
+	count += 1
+if n == 1:
+	print(n0, " is a happy number.")
+elif n == 0:
+	print(n0, " is a sad number.")
+else:
+	print(n0, " is neither a happy nor a sad number.")
