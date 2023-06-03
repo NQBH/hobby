dafae6ef854e2c533011e4ff2d8a604c147e923a
NQBH <nguyenquanbahong@gmail.com>
2023-06-03 21:06:45 +0700

    contracted terminal cmd

--- elementary_computer_science/C_Cpp/template.cpp
+++ elementary_computer_science/C_Cpp/template.cpp
@@ -4,11 +4,8 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	// Input & Output
 	freopen("prob.inp", "r", stdin);
 	freopen("prob.out", "w", stdout);
-	// 
 
 }
-// Compile in Terminal:
-// g++ -std=c++11 -O2 -Wall test.cpp -o test
+// Terminal: g++ -std=c++11 -O2 -Wall test.cpp -o test
