02739fa61c8ec412a2a8e03115c9f7eaa732dc2f
NQBH <nguyenquanbahong@gmail.com>
2023-06-04 13:38:14 +0700

    C++: maximum sum of subarray

--- elementary_computer_science/C_Cpp/max_subarray_sum.cpp
+++ elementary_computer_science/C_Cpp/max_subarray_sum.cpp
@@ -5,22 +5,25 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	int best;
+	int n = 7; // array size
+	int array[] = {4,2,5,3,5,8,3};
 	// O(n^3)
 	best = 0;
+	int sum;
 	for (int a = 0; a < n; ++a) {
 		for (int b = a; b < n; ++b) {
-			int sum = 0;
+			sum = 0;
 			for (int k = a; k <= b; ++k) {
 				sum += array[k];
 			}
 		}
-		best = max(best,sum);
+		best = max(best, sum);
 	}
 	cout << best << "\n";
 	// O(n^2)
 	best = 0;
 	for (int a = 0; a < n; ++a) {
-		int sum = 0;
+		sum = 0;
 		for (int b = a; b < n; ++b) {
 			sum += array[b];
 			best = max(best, sum);
@@ -29,12 +32,11 @@ int main() {
 	cout << best << "\n";
 	// O(n)
 	best = 0;
-	int sum = 0;
+	sum = 0;
 	for (int k = 0; k < n; ++k) {
 		sum = max(array[k], sum + array[k]);
 		best = max(best, sum);
 	}
 	cout << best << "\n";
 }
-// Compile in Terminal:
-// g++ -std=c++11 -O2 -Wall max_subarray_sum.cpp -o max_subarray_sum
+// Terminal: g++ -std=c++11 -O2 -Wall max_subarray_sum.cpp -o max_subarray_sum
