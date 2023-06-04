96160ac027893e0f520d55f3947219c04c7d7072
NQBH <nguyenquanbahong@gmail.com>
2023-06-04 10:34:15 +0700

    Python: lucky number: odd then even sequence

---
+++ elementary_computer_science/Python/lucky_number.py
@@ -0,0 +1,17 @@
+from math import floor
+def luck_number(n, k):
+	if k <= floor((n + 1)/2):
+		print(2*k - 1)
+	else:
+		print(2*(k - round((n + 1)/2) + 1))
+
+nk = input()
+nk = nk.split()
+n = int(nk[0])
+k = int(nk[1])
+luck_number(n, k)
+"""
+# Print all
+for i in range(1, k + 1):
+	luck_number(n, i)
+"""
