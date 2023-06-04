fb7df9273af0911f028a267e81eacb8e2961235a
NQBH <nguyenquanbahong@gmail.com>
2023-06-04 13:43:31 +0700

    added sort for ordinary array & string

--- elementary_computer_science/Cpp/sort.cpp
+++ elementary_computer_science/Cpp/sort.cpp
@@ -12,7 +12,18 @@ int main() {
 			}
 		}
 	}
-
+	// C++'s sort efficiently sort elements of a vector in increasing order
+	vector<int> v = {4,2,5,3,5,8,3};
+	sort(v.begin(), v.end());
+	// C++'s sort sorts decreasingly
+	sort(v.rbegin(), v.rend());
+	// Sort an ordinary array
+	int A[] = {4,2,5,3,5,8,3};
+	sort(A.begin(), A.end()) // sort(a, a + n); // original
+	// Sort a string
+	string S = "monkey";
+	sort(S.begin(), S.end());
+	return 1;
 }
 // Compile in Terminal:
 // g++ -std=c++11 -O2 -Wall sort.cpp -o sort
