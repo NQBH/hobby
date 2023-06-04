a47efde7798a8edce612061d6f6c633000704702
NQBH <nguyenquanbahong@gmail.com>
2023-06-04 10:13:42 +0700

    Python: check if perfect number & print all of its divisors

---
+++ elementary_computer_science/Python/perfect_number.py
@@ -0,0 +1,17 @@
+def is_perfect_number(n):
+	sum_proper_divisor = 0
+	list_proper_divisor = []
+	for i in range(1, round(n/2 + 1)):
+		if n % i == 0:
+			list_proper_divisor.append(i)
+			sum_proper_divisor += i
+	print(sum_proper_divisor)
+	if sum_proper_divisor == n:
+		list_proper_divisor.append(n)
+		print(n, " is a perfect number.")
+		print("List of of all divisor of ", n, ":", *list_proper_divisor)
+	else:
+		print(n, " is not a perfect number.")
+
+n = int(input())
+is_perfect_number(n)
