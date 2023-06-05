e68e73327e0ef0437f5a3f75ea940ed72053abc2
NQBH <nguyenquanbahong@gmail.com>
2023-06-05 23:00:25 +0700

    restored str print

--- elementary_computer_science/Python/HTP/divisible_by.py
+++ elementary_computer_science/Python/HTP/divisible_by.py
@@ -2,8 +2,8 @@ try:
 	a = int(input())
 	b = int(input())
 	if a % b == 0 :
-		print(a, ' chia het cho ', b, '.')
+		print(str(a) + ' chia het cho ' + str(b) + '.')
 	else:
-		print(a, ' khong chia het cho ', b, '.')
+		print(str(a) + ' khong chia het cho ' + str(b) + '.')
 except:
 	print('Day khong phai la 1 so nguyen.')
