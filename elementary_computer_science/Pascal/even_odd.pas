2706e512d6d3fb0a360e9286bb4907f635586d71
NQBH <nguyenquanbahong@gmail.com>
2023-06-05 22:24:29 +0700

    trimmed Pascal: even vs. odd

--- elementary_computer_science/Pascal/even_odd.pas
+++ elementary_computer_science/Pascal/even_odd.pas
@@ -1,7 +1,7 @@
 program even_odd;
 var n : integer;
 begin
-	write('Enter n = ');
+	write('n = ');
 	readln(n);
-	if n mod 2 = 0 then writeln(n,' is even') else writeln(n, ' is odd');
+	if n mod 2 = 0 then writeln(n,' is even.') else writeln(n, ' is odd.');
 end.
