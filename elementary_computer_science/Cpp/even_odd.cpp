babfa3cf25de88cd5d5ecd83ae849eb65c35fb69
NQBH <nguyenquanbahong@gmail.com>
2023-06-05 22:34:06 +0700

    C++: even vs. odd

--- elementary_computer_science/Cpp/even_odd.cpp
+++ elementary_computer_science/Cpp/even_odd.cpp
@@ -8,6 +8,9 @@ int main() {
 	else cout << n << " is odd.\n";
 	// 2nd solution
 	(n % 2 == 0) ? cout << n << " is even.\n" : cout << n << " is odd.\n";
+	// 3rd solution using bitwise AND
+	if ((n & 1) == 0) cout << n << " is even.\n";
+	else cout << n << " is odd.\n";
 	return 0;
 }
 // Terminal: g++ -std=c++11 -O2 -Wall even_odd.cpp -o even_odd
