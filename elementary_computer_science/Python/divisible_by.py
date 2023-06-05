16e5fcf209fa67b36f9801bcd353f3e8761bc2cd
NQBH <nguyenquanbahong@gmail.com>
2023-06-05 22:59:26 +0700

    added .

--- elementary_computer_science/Python/divisible_by.py
+++ elementary_computer_science/Python/divisible_by.py
@@ -1,6 +1,6 @@
 a = int(input())
 b = int(input())
 if a % b == 0:
-   print(a, ' is divisible by ', b)
+   print(a, ' is divisible by ', b, '.')
 else:
-   print(a, ' is not divisible by ', b)
+   print(a, ' is not divisible by ', b, '.')
