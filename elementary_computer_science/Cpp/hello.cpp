eae6cb0b0b80413e194bb10ab57f4a757d6a4040
NQBH <nguyenquanbahong@gmail.com>
2023-06-12 15:30:50 +0700

    C++: hello

--- elementary_computer_science/Cpp/hello.cpp
+++ elementary_computer_science/Cpp/hello.cpp
@@ -1,6 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
 int main() {
-	printf("hello\n");
+	cout << "Hello.\n";
 }
+// Terminal: g++ -std=c++11 -O2 -Wall hello.cpp -o hello
