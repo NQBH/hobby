d7f3d99fc252d1e69d6a1f1f50044cadc840fad9
NQBH <nguyenquanbahong@gmail.com>
2023-06-12 19:16:58 +0700

    C: area of circle

--- elementary_computer_science/C/area_circle.c
+++ elementary_computer_science/C/area_circle.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int main() {
-	#define Pi 3.14159
+	// #define Pi 3.14159
 	float R, area;
 	scanf("%f", &R);
-	if (R <= 0) {
-		printf("R < 0.");
-		exit(1);
+	if (R < 0) {
+		printf("Error: R < 0.");
+		exit(0);
 	}
-	area = Pi*R*R;
+	area = M_PI*R*R;
 	printf("The area of the circle whose radius R = %f is %f.\n", R, area);
 }
 // Terminal: gcc area_circle.c -o area_circle
