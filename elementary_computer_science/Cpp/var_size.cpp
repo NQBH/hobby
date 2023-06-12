4b3ac54d82aad19fc95c11e1c028e88f97f865ab
NQBH <nguyenquanbahong@gmail.com>
2023-06-12 19:56:28 +0700

    C++: size of variables

---
+++ elementary_computer_science/Cpp/var_size.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	short a = 9;
+	cout << "Size of variable a = " << sizeof(a) << '.' << '\n';
+	cout << "Size of int type = " << sizeof(int) << '.' << '\n';
+	cout << "Size of long type = " << sizeof(long) << '.' << '\n';
+	cout << "Size of float type = " << sizeof(float) << '.' << '\n';
+	cout << "Size of double type = " << sizeof(double) << '.' << '\n';
+	cout << "Size of char type = " << sizeof(char) << '.' << '\n';
+}
