7ff5c18111115b0995d2862693f7206815c8c46a
NQBH <nguyenquanbahong@gmail.com>
2023-06-12 19:41:26 +0700

    C: size of various types

---
+++ elementary_computer_science/C/var_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+	short a = 9;
+	printf("Size of variable a = %d.\n", sizeof(a));
+	printf("Size of int type = %d.\n", sizeof(int));
+	printf("Size of long type = %d.\n", sizeof(long));
+	printf("Size of float type = %d.\n", sizeof(float));
+	printf("Size of double type = %d.\n", sizeof(double));
+	printf("Size of char type = %d.\n", sizeof(char));
+}
+// Terminal: gcc var_size.c -o var_size
