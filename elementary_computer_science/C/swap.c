6945a88bfcbdf869c007620558ddf13cbeb5a9c4
NQBH <nguyenquanbahong@gmail.com>
2023-06-13 10:41:01 +0700

    C: swap

--- elementary_computer_science/C/swap.c
+++ elementary_computer_science/C/swap.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 void swap(float *x, float *y) {
 	float u = *x;
 	*x = *y;
 	*y = u;
 }
 
+void ajust(float *x, float *y) {
+	if (fabs(*x) > fabs(*y))
+		swap(x, y); // swap(&(*x), &(*y)); // <=>
+}
+
 int main() {
-	float a = 186, b = 273;
+	float a = 186, b = 273, A = -1864.5F, B = 273.9F;
 	swap(&a, &b);
 	printf("New values of a & b now are a = %f,  = %f.\n", a, b);
+	ajust(&A, &B);
+	printf("New values of A & B now are A = %f,  = %f.\n", A, B);
 	return 0;
 }
