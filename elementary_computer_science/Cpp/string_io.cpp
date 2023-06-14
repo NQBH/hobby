ea9b13b68977d1ad28842d79da438dffffb5887f
NQBH <nguyenquanbahong@gmail.com>
2023-06-14 19:48:36 +0700

    C++: input & output string with/without blank(s)

--- elementary_computer_science/Cpp/string_io.cpp
+++ elementary_computer_science/Cpp/string_io.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 #define MAXSZ 256
+#define gets_s(s,L) {fgets((s), (L) + 1, stdin); (s)[strlen(s) - 1] = 0;}
 int main() {
 	char str[MAXSZ + 1];
-	string str1, str2;
+	string str1, str2, str3;
 	cout << "1st string: ";
 	cin.getline(str, MAXSZ);
 	str1 = str;
@@ -11,6 +12,10 @@ int main() {
 	cout << "2nd string: ";
 	cin.getline(str, MAXSZ);
 	str2 = str;
-	cout << "Our strings are:\n" << str1.c_str() << '\n' << str2.c_str() << '\n';
+	cout << "String with no blank: ";
+	cin >> str;
+	str3 = str;
+	fflush(NULL); // _flushall();
+	cout << "Our strings are:\n" << str1.c_str() << '\n' << str2.c_str() << '\n' << str3 << '\n';
 	return 1;
 }
