7540cdc71da2f6ea31b6e79235df5b9ad59e65a1
NQBH <nguyenquanbahong@gmail.com>
2023-06-15 23:56:39 +0700

    C++: set width of number & string

---
+++ elementary_computer_science/Cpp/set_width.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	int area = 970, height = 10, volume = 9700;
+	cout << setw(8) << "Area" << setw(10) << area << ".\n";
+	cout << setw(8) << "Height" << setw(10) << height << ".\n";
+	cout << setw(8) << "Volume" << setw(10) << volume << ".\n";
+}
