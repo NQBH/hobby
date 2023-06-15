a1d8465aefb19af27d8df98f39bcad126c2e658e
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 00:01:42 +0700

    C++: hex & oct formats

---
+++ elementary_computer_science/Cpp/hex_oct_format.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+	long n;
+	cout << "n (hexadecimal) = ";
+	cin >> hex >> n;
+	cout << "Octal representation: " << oct << n << ".\n";
+}
