77a825ff99459962ef1942948928375173c013ca
NQBH <nguyenquanbahong@gmail.com>
2023-06-15 23:29:49 +0700

    C++: special cubic eqn

--- elementary_computer_science/Cpp/special_cubic_eqn.cpp
+++ elementary_computer_science/Cpp/special_cubic_eqn.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 using namespace std;
 int main() {
-	double p, q, y, delta;
-	cout << "p = ";
-	cin >> p;
-	cout << "q = ";
-	cin >> q;
-	delta = pow(p, 6.0) + pow(q, 2.0);
+	double a, b, x, delta;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+	delta = pow(a, 6.0) + pow(b, 2.0);
 	delta = sqrt(delta);
-	y = pow(delta - q, 1.0/3) - pow(delta + q, 1.0/3);
-	cout << "Root y = " << y << '\n';
-	double test = y*y*y + 3*p*p*y + 2*q;
-	cout << "y^3 + 3p^2y + 2q = " << test << '\n';
-	return 1;
+	x = pow(delta - b, 1.0/3) - pow(delta + b, 1.0/3);
+	cout << "Root x = " << x << ".\n";
+	double test = x*x*x + 3*a*a*x + 2*b;
+	cout << "x^3 + 3a^2x + 2b = " << test << ".\n";
 }
