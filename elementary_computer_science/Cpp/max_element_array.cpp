2e2822295560fcb8c01167bb3404a77275ce9a65
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 00:30:51 +0700

    C++: maximum element of an array

---
+++ elementary_computer_science/Cpp/max_element_array.cpp
@@ -0,0 +1,11 @@
+#define Max(x, y) ( ( (x) > (y) )? (x) : (y) )
+#define MaxArray(a, n, mm) { mm = a[0]; for (int i = 1; i < n; ++i) { mm = Max(mm, a[i]);} }
+#include <iostream>
+using namespace std;
+int main() {
+	double u[] = { 7.3, 11.5, 23.6, -6.8, -8.67, 23.599 };
+	double max = 0;
+	int n = sizeof(u)/sizeof(u[0]);
+	MaxArray(u, n, max);
+	cout << "Maximum number is " << max << ".\n";
+}
