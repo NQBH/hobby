a4e50a69f1e412261f553fe0a3d415e4b2f954bc
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 00:21:02 +0700

    C++: macro math elements

---
+++ elementary_computer_science/Cpp/macro_elements.h
@@ -0,0 +1,9 @@
+#define E 2.71828
+#define Ce 0.577216
+#define PI 3.14159
+#define Delta(a, b, c) ( (b)*(b) - 4*(a)*(c) )
+#define ABS(x) ( (x > 0)?(x): -(x) )
+#define Lamda(x) (!(!(x))) // Lamda (x) ( ( (x) != 0)?1 : 0)
+#define Max(x, y) ( ( (x) > (y) )? (x) : (y))
+#define Min(x, y) ( ( (x) < (y) )? (x) : (y))
+#define Sign(x) ( ( (x) > 0 )? 1: ( ( (x) < 0)? -1 : 0) )
