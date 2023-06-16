e6111063a533d691aea4c44e33c781dd7c45abea
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 21:00:16 +0700

    C: added do while option

--- elementary_computer_science/C/sum_positive.c
+++ elementary_computer_science/C/sum_positive.c
@@ -8,4 +8,12 @@ int main() {
 			sum += x;
 	}
 	printf("Sum all positive numbers input = %f.\n", sum);
+	sum = 0;
+	do {
+		printf("Input a positive number: ");
+		scanf("%f", &x);
+		if (x > 0)
+			sum += x;
+	} while (x > 0);
+	printf("Sum all positive numbers input = %f.\n", sum);
 }
