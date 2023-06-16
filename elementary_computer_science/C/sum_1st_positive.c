6d0bf06ce79709be6efc025d65984fbbd80e56a6
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 21:05:09 +0700

    added do while option

--- elementary_computer_science/C/sum_1st_positive.c
+++ elementary_computer_science/C/sum_1st_positive.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 int main() {
-	long n, sum, k;
+	long n, sum = 0, k = 1;
 	printf("Input n = ");
 	scanf("%ld", &n);
-	sum = 0, k = 1;
 	while (sum + k <= n) {
 		sum += k;
 		++k;
 	}
 	--k;
+	/* // do while option
+	do {
+		sum += k;
+		++k;
+	} while (sum < n);
+	if (sum > n) {
+		sum -= k;
+		--k
+	}
+	*/	
 	printf("k = %ld.\n", k);
 	printf("Sum of 1st k positive integers = %ld.\n", sum);
 	if (sum == n)
