589d165729e2da5bf3334fb85268af951efdd6d6
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 20:20:20 +0700

    updated C++: subset generation

--- elementary_computer_science/Cpp/subset_generation.cpp
+++ elementary_computer_science/Cpp/subset_generation.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 void search(int k) {
 	if (k == n + 1) {
 		// process subset
@@ -18,7 +17,4 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	vector<int> subset;
-
 }
-// Compile in Terminal:
-// g++ -std=c++11 -O2 -Wall subset_generation.cpp -o subset_generation
