30f98b24b7a33e38564ef2249a43c4889de1d6ed
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 20:55:53 +0700

    C++: sum of 1st positive ints

---
+++ elementary_computer_science/Cpp/sum_1st_positive.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	long n, sum = 0, k = 1;
+	cout << "Input n = ";
+	cin >> n;
+	sum = 0, k = 1;
+	while (sum + k <= n) {
+		sum += k;
+		++k;
+	}
+	--k;
+	cout << "k = " << k << ".\n";
+	cout << "Sum of 1st k positive integers = " << sum << ".\n";
+	if (sum == n)
+		cout << n << " is the sum of 1st k positive integers.\n";
+	else
+		cout << n << " is not the sum of 1st k positive integers.\n";
+}
