97facdeed13abd7bca948271d980faaa2ba6a86c
NQBH <nguyenquanbahong@gmail.com>
2023-06-16 20:45:51 +0700

    C++: sum of positives input

---
+++ elementary_computer_science/Cpp/sum_positive.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float sum = 0, x = 1;
+	while (x > 0) {
+		cout << "Input a positive number: ";
+		cin >> x;
+		if (x > 0)
+			sum += x;
+	}
+	cout << "Sum all positive numbers input = " << sum << ".\n";
+}
