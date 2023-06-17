c1a53e98ebf508da6730c203922eefa2afca26e1
NQBH <nguyenquanbahong@gmail.com>
2023-06-17 20:07:09 +0700

    added roundwith n-digit precision

--- elementary_computer_science/Cpp/round.cpp
+++ elementary_computer_science/Cpp/round.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 using namespace std;
 double round(double x); // Declare function round()
+double round(double x, int n); // Declare function round() with n-digit precision
+
 double round(double x) {
 	double ans;
 	if (x >= 0)
@@ -10,10 +12,23 @@ double round(double x) {
 		ans = -floor(-x + 0.5);
 	return ans;
 }
+
+double round(double x, int n) {
+	double ans, scale = pow(10, n);
+	x *= scale;
+	if (x >= 0)
+		ans = floor(x + 0.5)/scale;
+	else
+		ans = -floor(-x + 0.5)/scale;
+	return ans;
+}
+
 int main() {
-	double a;
+	double a; int n;
 	cout << "Input a = ";
 	cin >> a;
-	double y = round(a);
-	cout << "round(" << a << ") = " << y << ".\n";
+	cout << "Round to n = ";
+	cin >> n;
+	cout << "round(" << a << ") = " << round(a) << ".\n";
+	cout << "round(" << a << "," << n << ") = " << round(a, n) << ".\n";
 }
