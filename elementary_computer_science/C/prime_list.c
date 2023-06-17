4bcf4f57165d4793c145ce6a26d0b620d5485009
NQBH <nguyenquanbahong@gmail.com>
2023-06-17 23:59:16 +0700

    C++: list of primes

---
+++ elementary_computer_science/C/prime_list.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_prime(long n) {
+	int prime;
+	if (n < 2)
+		prime = 0;
+	else {
+		long i = 2;
+		double sqrtN = sqrt(n);
+		prime = 1;
+		while (i <= sqrtN) {
+			if (n % i == 0) {
+				prime = 0;
+				break;
+			}
+			++i;
+		}
+
+	}
+	return prime;
+}
+
+void prime_list(long n) {
+	for (long i = 2; i <= n; ++i) {
+		if (is_prime(i))
+			printf("%ld ", i);
+	}
+	printf("\n");
+}
+
+void prime_list_odd(long n) {
+	printf("2 ");
+	for (long i = 3; i <= n; i += 2) {
+		if (is_prime(i))
+			printf("%ld ", i);
+	}
+	printf("\n");
+}
+
+int main() {
+	long n;
+	printf("Input n = ");
+	scanf("%ld", &n);
+	printf("isprime(%ld) = %d.\n", n, is_prime(n));
+	prime_list(n);
+	prime_list_odd(n);
+}
