0602f962e2569db8176686aaf598e176e0ff525d
NQBH <nguyenquanbahong@gmail.com>
2023-06-18 18:32:09 +0700

    updated Python: Goldbach conjecture

--- elementary_computer_science/Python/Goldbach_conjecture.py
+++ elementary_computer_science/Python/Goldbach_conjecture.py
@@ -1,5 +1,4 @@
 from math import sqrt
-from math import floor
 def is_prime(x):
 	if x == 1:
 		return 0
@@ -7,14 +6,13 @@ def is_prime(x):
 		if x % i == 0:
 			return 0
 	return 1
-
 t = int(input())
 for i in range(t):
 	n = int(input())
-	if n % 2 != 0 and is_prime(n - 2) == 1:
-		print(n, " = 2 + ", n - 2)
+	if n % 2 != 0:
+		print(f'{n} is odd. Must input an even natural number.')
 	else:
-		for j in range(3, floor(n/2)):
+		for j in range(3, n//2):
 			if is_prime(j) == 1 and is_prime(n - j) == 1:
 				print(n, " = ", j, " + ", n - j)
-				# break # if want only 1 representation
+				# break # uncomment if want only 1 representation, comment if want all satisfied representations
