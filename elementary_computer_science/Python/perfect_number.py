ac03fade5b840af9b382448be8fb481930d776df
NQBH <nguyenquanbahong@gmail.com>
2023-06-18 19:51:52 +0700

    Python: perfect number

--- elementary_computer_science/Python/perfect_number.py
+++ elementary_computer_science/Python/perfect_number.py
@@ -1,16 +1,15 @@
 def is_perfect_number(n):
 	sum_proper_divisor = 0
-	list_proper_divisor = []
+	list_divisor = []
 	for i in range(1, round(n/2 + 1)):
 		if n % i == 0:
-			list_proper_divisor.append(i)
+			list_divisor.append(i)
 			sum_proper_divisor += i
 	if sum_proper_divisor == n:
-		list_proper_divisor.append(n)
+		list_divisor.append(n)
 		print(n, " is a perfect number.")
-		print("List of of all divisor of ", n, ":", *list_proper_divisor)
+		print(f'List of of all divisor of {n}:', *list_divisor, '.')
 	else:
-		print(n, " is not a perfect number.")
-
+		print(f'{n} is not a perfect number.')
 n = int(input())
 is_perfect_number(n)
