a5536dcf66213561e4861dcc1f196bd275a40a89
NQBH <nguyenquanbahong@gmail.com>
2023-06-21 21:34:56 +0700

    C++: solve function eqn with function parameter

--- elementary_computer_science/Cpp/approximate_solution.cpp
+++ elementary_computer_science/Cpp/approximate_solution.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+const double epsilon = 0.000000001;
 double Func(double x) {
 	return pow(x, 9) + x + 1;
 }
@@ -10,8 +10,15 @@ double Func(double x, int n) {
 	return pow(x, 2*n + 1) + x + 1;
 }
 
+double Func1(double x) {
+	return pow(x, 5) + 7*x + 1; // Function x^5 + 7x  + 1
+}
+
+double Func2(double x) {
+	return 2*pow(x, 11) + 17*pow(x, 5) + 1; // Function 2x^11 + 17x^5  + 1
+}
+
 void Solve(double& x) {
-	const double epsilon = 0.000000001;
 	double left = -1, right = 0;
 	while (right - left > epsilon) {
 		double mid = (left + right)/2;
@@ -24,7 +31,6 @@ void Solve(double& x) {
 }
 
 void Solve(double& x, int n) {
-	const double epsilon = 0.000000001;
 	double left = -1, right = 0;
 	while (right - left > epsilon) {
 		double mid = (left + right)/2;
@@ -34,6 +40,18 @@ void Solve(double& x, int n) {
 			left = mid;
 	}
 	x = (left + right)/2;
+}
+
+double Solve(double F(double x), double a, double b) {
+	double left = a, right = b;
+	while (right - left > epsilon) {
+		double mid = (left + right)/2;
+		if (F(left)*F(mid) < 0)
+			right = mid;
+		else
+			left = mid;
+	}
+	return (left + right)/2;
 }
 
 int main() {
@@ -46,4 +64,10 @@ int main() {
 	Solve(x, n);
 	cout << "x = " << x << ".\n";
 	cout << "Test: " << Func(x, n) << ".\n";
+	x = Solve(Func1, -1, 0);
+	cout << "1st equation, x = " << x << ".\n";
+	cout << "Test: " << Func1(x) << ".\n";
+	x = Solve(Func2, -1, 0);
+	cout << "2nd equation, x = " << x << ".\n";
+	cout << "Test: " << Func2(x) << ".\n";
 }
