59e8af9434bc13744c44cded8fea2d0f12053ea6
NQBH <nguyenquanbahong@gmail.com>
2023-06-21 20:07:35 +0700

    C: approximate solution by bisection method

---
+++ elementary_computer_science/C/approximate_solution.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+double Func(double x) {
+	return pow(x, 9) + x + 1;
+}
+
+void Solve(double *x) {
+	const double epsilon = 0.000000001;
+	double left = -1, right = 0;
+	while (right - left > epsilon) {
+		double mid = (left + right)/2;
+		if (Func(left)*Func(mid) < 0)
+			right = mid;
+		else
+			left = mid;
+	}
+	*x = (left + right)/2;
+}
+
+int main() {
+	double x;
+	Solve(&x);
+	printf("x = %lf.\n", x);
+	printf("Test: %lf.\n", Func(x));
+}
+/*
+nqbh@nqbh-msi:~/hobby/elementary_computer_science/C$ gcc approximate_solution.c -o approximate_solution
+/usr/bin/ld: /tmp/ccq7gbyS.o: in function `Func':
+approximate_solution.c:(.text+0x27): undefined reference to `pow'
+collect2: error: ld returned 1 exit status
+nqbh@nqbh-msi:~/hobby/elementary_computer_science/C$ g++ approximate_solution.c -o approximate_solution
+*/
