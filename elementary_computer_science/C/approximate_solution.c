b379a863f901454afe30beb9b0f8d5331804b374
NQBH <nguyenquanbahong@gmail.com>
2023-06-21 20:18:33 +0700

    C: solve eqn x^{2n+1} + x + 1

--- elementary_computer_science/C/approximate_solution.c
+++ elementary_computer_science/C/approximate_solution.c
@@ -5,6 +5,10 @@ double Func(double x) {
 	return pow(x, 9) + x + 1;
 }
 
+double Func_n(double x, int n) {
+	return pow(x, 2*n + 1) + x + 1;
+}
+
 void Solve(double *x) {
 	const double epsilon = 0.000000001;
 	double left = -1, right = 0;
@@ -18,11 +22,31 @@ void Solve(double *x) {
 	*x = (left + right)/2;
 }
 
+void Solve_n(double *x, int n) {
+	const double epsilon = 0.000000001;
+	double left = -1, right = 0;
+	while (right - left > epsilon) {
+		double mid = (left + right)/2;
+		if (Func_n(left, n)*Func_n(mid, n) < 0)
+			right = mid;
+		else
+			left = mid;
+	}
+	*x = (left + right)/2;
+}
+
 int main() {
 	double x;
+	int n;
+	printf("n = ");
+	scanf("%d", &n);
 	Solve(&x);
 	printf("x = %lf.\n", x);
 	printf("Test: %lf.\n", Func(x));
+	Solve_n(&x, n);
+	printf("x = %lf.\n", x);
+	printf("Test: %lf.\n", Func_n(x, n));
+
 }
 /*
 nqbh@nqbh-msi:~/hobby/elementary_computer_science/C$ gcc approximate_solution.c -o approximate_solution
