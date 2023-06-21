f14e4481b01311a2eb79be39263a92d49d149ccb
NQBH <nguyenquanbahong@gmail.com>
2023-06-21 20:53:19 +0700

    added swap doubles & swap ints

--- elementary_computer_science/Cpp/swap.cpp
+++ elementary_computer_science/Cpp/swap.cpp
@@ -1,4 +1,17 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+void Swap(double& a, double& b) {
+	double c = a;
+	a = b;
+	b = c;
+}
+
+void Swap(int& a, int& b) {
+	int c = a;
+	a = b;
+	b = c;
+}
+
 template <class T>
 void swap(T *x, T *y) {
 	T u = *x;
@@ -16,6 +29,11 @@ int main() {
 	float a = 186, b = 273;
 	swap(&a, &b);
 	printf("New values of a & b now are a = %f,  = %f.\n", a, b);
-	return 0;
+	double x = 89.51345, y = 72.89865;
+	Swap(x, y);
+	cout << "After swapping: x = " << x << ", y = " << y << ".\n";
+	int u = 21, v = 37;
+	Swap(u, v);
+	cout << "After swapping: u = " << u << ", v = " << v << ".\n";
 }
 // g++ -std=c++20 -O2 -Wall swap.cpp -o swap
