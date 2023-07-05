168558f13671530bef045555315e810838ea24a6
NQBH <nguyenquanbahong@gmail.com>
2023-07-05 23:41:59 +0700

    C++: leap year

--- elementary_computer_science/Cpp/leap_year.cpp
+++ elementary_computer_science/Cpp/leap_year.cpp
@@ -23,7 +23,7 @@ int check_leap_year(int year) {
 	return ans;
 }
 
-int check_leap_year_short(int year) {
+int check_leap_year1(int year) {
 	int ans;
 	if (year < min_year || year > max_year)
 		ans = -1;
@@ -38,8 +38,25 @@ int check_leap_year_short(int year) {
 	return ans;
 }
 
+int check_leap_year2(int year) {
+	int ans;
+	if (year < min_year || year > max_year)
+		ans = -1;
+	else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+		ans = 1;
+	else
+		ans = 0;
+	return ans;
+}
+
+int check_leap_year3(int year) {
+	if (year < min_year || year > max_year)
+		return -1;
+	else return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
 int main() {
 	int n;
 	cin >> n;
-	cout << "Leap year: " << check_leap_year(n) << ", " << check_leap_year_short(n) << ".\n";
+	cout << "Leap year: " << check_leap_year(n) << ", " << check_leap_year1(n) << ", " << check_leap_year2(n) << ", " << check_leap_year3(n) << ".\n";
 }
