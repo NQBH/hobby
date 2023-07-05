4a76212315e70f4d830fb23b18a13c38dd5af80d
NQBH <nguyenquanbahong@gmail.com>
2023-07-05 20:51:30 +0700

    Python: powerful number

--- elementary_computer_science/Python/powerful_number.py
+++ elementary_computer_science/Python/powerful_number.py
@@ -1,16 +1,14 @@
-from math import *
-# Function: Check prime 
+from math import sqrt
 def is_prime(x):
 	if x == 1: return 0
 	for i in range(2, int(sqrt(x)) + 1):
 		if x % i == 0: return 0
 	return 1
-
 s = ""
 n = 1
 while n <= 1000:
 	flag = 0
-	for i in range(2, n):
+	for i in range(1, n):
 		if is_prime(i) and n % i == 0:
 			if n % (i*i) != 0:
 				flag = 0
