6d4e325e1995afbf4431b7d1df73e3ea41c4495f
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 09:00:49 +0700

    C++: electricity bill with VAT

--- elementary_computer_science/Cpp/electricity_bill.cpp
+++ elementary_computer_science/Cpp/electricity_bill.cpp
@@ -8,6 +8,7 @@ static double temp1 = (Lmt1 - 0)*Prc1,
 		temp3 = temp2 + (Lmt3 - Lmt2)*Prc3,
 		temp4 = temp3 + (Lmt4 - Lmt3)*Prc4,
 		temp5 = temp4 + (Lmt5 - Lmt4)*Prc5;
+// 1st electricity bill function
 double electricity_bill(int kWh) {
 	double sum_money = 0;
 	if (kWh <= Lmt1)
@@ -26,9 +27,30 @@ double electricity_bill(int kWh) {
 	sum_money = (1 + VAT_rate)*sum_money;
 	return sum_money;
 }
+// 2nd electricity bill function
+const int _MyExtreme = -1;
+double subCompute(int Lm1, int Lm2, double Prc, int kWh) {
+	double value = 0;
+	if (kWh >= Lm1) {
+		if (kWh < Lm2 || Lm2 == _MyExtreme)
+			value = (kWh - Lm1)*Prc;
+		else
+			value = (Lm2 - Lm1)*Prc;
+	}
+	return value;
+}
+
+double electricity_bill_improved(int kWh) {
+	double sum_money = subCompute(0, Lmt1, Prc1, kWh) + subCompute(Lmt1, Lmt2, Prc2, kWh)
+					 + subCompute(Lmt2, Lmt3, Prc3, kWh) + subCompute(Lmt3, Lmt4, Prc4, kWh)
+					 + subCompute(Lmt4, Lmt5, Prc5, kWh) + subCompute(Lmt5, _MyExtreme, Prc6, kWh);
+	// Compute VAT
+	sum_money = (1 + VAT_rate)*sum_money;
+	return sum_money;
+}
 
 int main() {
 	int kWh;
 	cin >> kWh;
-	cout << electricity_bill(kWh) << ".\n";
+	cout << electricity_bill(kWh) << ".\n" << electricity_bill_improved(kWh) << ".\n";
 }
