fb1d7c79597ff85e69137c270a21e60668a45b12
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 16:17:04 +0700

    C: input 2D array

--- elementary_computer_science/C/array_2D.c
+++ elementary_computer_science/C/array_2D.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #define num_row 5 // const int num_row = 5;
 #define num_col 7 // const int num_col = 7;
+#define max_row 20 // const int max_row = 5;
+#define max_col 20 // const int max_col = 7;
 
 int main() {
 	int a[num_row][num_col], i, j;
@@ -9,4 +11,16 @@ int main() {
 	for (i = 0; i < num_row; ++i)
 		for (j = 0; j < num_col; ++j)
 			a[i][j] = rand() % 100;
+	int b[max_row][max_col], mRow, nCol;
+	printf("mRow = ");
+	scanf("%d", &mRow);
+	printf("nCol = ");
+	scanf("%d", &nCol);
+	if (mRow < 1 || mRow > max_row || nCol < 1 || nCol > max_col) {
+		printf("Invalid size!\n");
+		return;
+	}
+	for (i = 0; i < mRow; ++i)
+		for (j = 0; j < nCol; ++j)
+			scanf("%d", &a[i][j]);
 }
