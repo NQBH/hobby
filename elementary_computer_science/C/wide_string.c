6a1e1aec6c58e86683ce4f7a5853dc6095036b14
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 18:53:25 +0700

    C: wide string str_to_int64

--- elementary_computer_science/C/wide_string.c
+++ elementary_computer_science/C/wide_string.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <wchar.h>
+#include <stdint.h> // To use int type int64_t
 #define MAXSZ 256
+
+int64_t str_to_int64(char* str) {
+	int64_t ans;
+	sscanf(str, "%lld", &ans);
+	return ans;
+}
+
+int64_t str_to_int64(wchar_t* wstr) {
+	int64_t ans;
+	swscanf(wstr, L"%lld", &ans);
+	return ans;
+}
 
 int main() {
 	char str[MAXSZ];
@@ -9,4 +22,12 @@ int main() {
 	sprintf(str, "Point (%f, %f)\n", x, y);
 	swprintf(wstr, MAXSZ, L"Point (%f, %f)\n", x, y);
 	// str & wstr are ready for use to output to files, printers, or graphics screen ...
+	
+	char str1[] = "12345678910"; // 11 characters, exceed number of digits of long
+	wchar_t wstr1[] = L"1110987654321"; // 13 characters, exceed number of digits of long
+	int64_t m, n;
+	m = str_to_int64(str1);
+	n = str_to_int64(wstr1);
+	sscanf("(-21.625, 38.125)", "(%f, %f)", &x, &y);
+	printf("Results m = %lld, n = %lld, x = %f, y = %f\n", m, n, x, y);
 }
