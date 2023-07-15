ab5e54f06445ee1cf22af33cd19897f77e5aec74
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 14:06:33 +0700

    C: input array of ints

---
+++ elementary_computer_science/C/array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define N 50 // const int N = 50;
+void array_int_input(int a[N], int& n) {
+	while (1) {
+		printf("Number of elements will be used (<= %d): ", N);
+		scanf("%d", &n);
+		if ((n < 0) || (n > N))
+			printf("Wrong input, reinput ...\n");
+		else
+			break;
+	}
+	for (int i = 0; i < n; ++i) {
+		printf("a[%d] = ", i);
+		scanf("%d", &a[i]);
+	}
+}
+
+int main() {
+	int n = 10, a[N];
+	array_int_input(a, n);
+}
