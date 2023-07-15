25295aa100f5be701bc732a8cbe6a5b55b602e97
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 20:57:42 +0700

    Python: penalty

--- elementary_computer_science/Python/penalty.py
+++ elementary_computer_science/Python/penalty.py
@@ -2,7 +2,7 @@ t = int(input())
 for _ in range(t):
 	T0, Q0, L0 = map(int, input().split())
 	T, Q, L = T0, Q0, L0
-	while L > 1:
+	while T and Q and L > 1:
 		if T % L == 0:
 			T -= 1
 		elif Q % L == 0:
