5c0a5d6fd9cfed0819ca239ee60deb354ea6e266
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 14:29:19 +0700

    C: array input & output

--- elementary_computer_science/C/array.c
+++ elementary_computer_science/C/array.c
@@ -15,7 +15,15 @@ void array_int_input(int a[N], int& n) {
 	}
 }
 
+void array_int_output(int a[N], int n) {
+	printf("a[]:");
+	for (int i = 0; i < n; ++i)
+		printf(" %d", a[i]);
+	printf(".\n");
+}
+
 int main() {
 	int n = 10, a[N];
 	array_int_input(a, n);
+	array_int_output(a, n);
 }
