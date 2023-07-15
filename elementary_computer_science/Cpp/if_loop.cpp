6519d637fe1e94a1f0f59e098a5c8b6ea8945805
NQBH <nguyenquanbahong@gmail.com>
2023-07-15 13:18:58 +0700

    C++: examples of if loop

---
+++ elementary_computer_science/Cpp/if_loop.cpp
@@ -0,0 +1,24 @@
+int Collaz(int n) {
+	return (n % 2)*(3*n + 1) + (1 - n % 2)*(n/2);
+}
+
+double Abs(double a) {
+	return 2*(a >= 0)*a  -a;
+}
+
+double Max(double a, double b) {
+	// return (a > b)*a + (a <= b)*b;
+	return (a > b)*(a - b) + b;
+}
+
+double Min(double a, double b) {
+	// return (a > b)*b + (a <= b)*a;
+	return (a > b)*(b - a) + a;
+}
+
+int num_day_of_month(int month, int year) {
+	if (month < 1 || month > 12 || year < 0)
+		return -1;
+	return 28 + (month != 2)*(2 + (month < 8)*(month % 2) + (month >= 8)*(1 - month % 2))
+			  + (month == 2)*((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+}
