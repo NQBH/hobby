ffd9d2fcd5bdb3e76bd2b8c4348e3d583f39f799
NQBH <nguyenquanbahong@gmail.com>
2023-07-30 15:02:46 +0700

    C: compute sum (-1)^{n-1}x^n/n

--- elementary_computer_science/C/sum_exp.c
+++ elementary_computer_science/C/sum_exp.c
@@ -9,13 +9,27 @@ double sum_exp(double x, int n) {
 	return Sn;
 }
 
+double sum_exp_minus(double x, int n) {
+	double Sn = 0, y = -1;
+	x = -x;
+	for (int i = 1; i <= n; ++i) {
+		y *= x;
+		Sn += y/i;
+	}
+	return Sn;
+}
+
 void main()
 {
-	double x, Sn; int n;
+	double x, Sn, En; int n;
 	printf("Input x: ");
 	scanf("%lf", &x);
 	printf("Input n: ");
 	scanf("%d", &n);
 	Sn = sum_exp(x, n);
-	printf("Result: %lf.\n", Sn);
+	printf("S(%d) = %lf.\n", n, Sn);
+	En = sum_exp_minus(x, n); // 1st sol: use new func
+	printf("E(%d) = %lf.\n", n, En);
+	En = -sum_exp(-x, n); // 2nd sol: use old func
+	printf("E(%d) = %lf.\n", n, En);
 }
