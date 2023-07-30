fe1a70ce9a78f2a3d08126bc68a2437e6cd8fccd
NQBH <nguyenquanbahong@gmail.com>
2023-07-30 14:39:23 +0700

    added func compute n!

--- elementary_computer_science/Cpp/sum_1st_positive.cpp
+++ elementary_computer_science/Cpp/sum_1st_positive.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
+
+void sum_product_1(long n, long& S, long& P) {
+	S = 0; P = 1;
+	for (int i = 1; i < n; ++i) {
+		S += i;
+		P *= i;
+	}
+}
+
+void sum_product_2(long n, long& S, long& P) {
+	S = 0; P = 1;
+	for(; n >= 1; --n) {
+		S += n;
+		P *= n;
+	}
+}
+
 int main() {
 	long n, sum = 0, k = 1;
 	cout << "Input n = ";
