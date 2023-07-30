25fc70cfa2acfa8ea7d2d4bd0e2d4f9e5d858dba
NQBH <nguyenquanbahong@gmail.com>
2023-07-30 15:05:19 +0700

    C++: compute sum (-1)^{n-1}x^n/n

--- elementary_computer_science/Cpp/sum_exp.cpp
+++ elementary_computer_science/Cpp/sum_exp.cpp
@@ -10,6 +10,16 @@ double sum_exp(double x, int n) {
 	return Sn;
 }
 
+double sum_exp_minus(double x, int n) {
+	double En = 0, y = -1;
+	x = -x;
+	for (int i = 1; i <= n; ++i) {
+		y *= x;
+		En += y/i;
+	}
+	return En;
+}
+
 int main() {
 	double x; int n;
 	cout << "Input x: ";
@@ -17,5 +27,9 @@ int main() {
 	cout << "Input n: ";
 	cin >> n;
 	double Sn = sum_exp(x, n);
-	cout << "Result: " << Sn << ".\n";
+	cout << "S(" << n << ") = " << Sn << ".\n";
+	double En = sum_exp_minus(x, n);
+	cout << "E(" << n << ") = " << En << ".\n";
+	En = -sum_exp(-x, n);
+	cout << "E(" << n << ") = " << En << ".\n";
 }
