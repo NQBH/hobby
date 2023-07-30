36ef5b9231e17464999c25fa114ee01d397c6b31
NQBH <nguyenquanbahong@gmail.com>
2023-07-30 15:34:04 +0700

    C: compute sum_{i=0}^n (-1)^i*x^i/i!

--- elementary_computer_science/C/sum_exp.c
+++ elementary_computer_science/C/sum_exp.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+// S_n(x) = sum_{i=0}^n x^i/i
 double sum_exp(double x, int n) {
 	double Sn = 0, y = 1;
 	for (int i = 1; i <= n; ++i) {
@@ -8,7 +8,7 @@ double sum_exp(double x, int n) {
 	}
 	return Sn;
 }
-
+// E_n(x) = sum_{i=0}^n (-1)^{i-1}*x^i/i
 double sum_exp_minus(double x, int n) {
 	double En = 0, y = -1;
 	x = -x;
@@ -18,7 +18,7 @@ double sum_exp_minus(double x, int n) {
 	}
 	return En;
 }
-
+// Exp_n(x) = sum_{i=0}^n x^i/i!
 double Exp(double x, int n) {
 	double Expn = 1, y = 1;
 	for (int i = 1; i <= n; ++i) {
@@ -30,17 +30,17 @@ double Exp(double x, int n) {
 
 void main()
 {
-	double x, Sn, En, Expn; int n;
+	double x, Sn, En, Kn; int n;
 	printf("Input x: ");
 	scanf("%lf", &x);
 	printf("Input n: ");
 	scanf("%d", &n);
 	Sn = sum_exp(x, n);
-	printf("S(%d) = %lf.\n", n, Sn);
+	printf("S(%lf, %d) = %lf.\n", x, n, Sn);
 	En = sum_exp_minus(x, n); // 1st sol: use new func
-	printf("E(%d) = %lf.\n", n, En);
+	printf("E(%lf, %d) = %lf.\n", x, n, En);
 	En = -sum_exp(-x, n); // 2nd sol: use old func
-	printf("E(%d) = %lf.\n", n, En);
-	Expn = Exp(-x, n);
-	printf("Exp(%d) = %lf.\n", n, Expn);
+	printf("E(%lf, %d) = %lf.\n", x, n, En);
+	Kn = Exp(-x, n); // K_n(x) = sum_{i=0}^n (-1)^i*x^i/i!
+	printf("K(%lf, %d) = %lf.\n", x, n, Kn);
 }
