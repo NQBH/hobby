7ce2f358d55cf3320f339647e465705e3a4003b5
NQBH <nguyenquanbahong@gmail.com>
2023-08-03 21:30:28 +0700

    Python: triangle number

--- elementary_computer_science/Python/triangle_number.py
+++ elementary_computer_science/Python/triangle_number.py
@@ -3,7 +3,8 @@ t = int(input())
 while t != 0:
 	t -= 1
 	n = int(input())
-	# 1st solution. Author: NQBH
+	"""
+	# 1st solution
 	i = 1
 	sum = 0
 	while sum < n:
@@ -13,23 +14,10 @@ while t != 0:
 		print(1)
 	else:
 		print(0)
-	# 2nd solution. Author: NQBH
-	tmp = floor(sqrt(2*n))
-	if n == tmp*(tmp + 1)/2:
+	"""
+	# 2nd solution
+	k = floor(sqrt(2*n))
+	if n == k*(k + 1)/2:
 		print(1)
 	else:
 		print(0)
-	# 3rd solution: Nguyen Tien Duc
-	a = 1
-	b = 1
-	c = -2*n
-	d = b*b - 4*a*c
-	x1 = (-b + sqrt(d))/(2*a)
-	x2 = (-b - sqrt(d))/(2*a)
-	if x1 > 0 and floor(x1) == x1:
-		print(1)
-		continue
-	if x2 > 0 and floor(x2) == x2:
-		print(1)
-		continue
-	print(0)
