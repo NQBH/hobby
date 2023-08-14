e93c097a3f4ac67ea1f8bb31f3ad4c92aef7c5c0
NQBH <nguyenquanbahong@gmail.com>
2023-08-14 09:31:55 +0700

    C++: lucky tower

--- elementary_computer_science/Cpp/HTP/lucky_tower.cpp
+++ elementary_computer_science/Cpp/HTP/lucky_tower.cpp
@@ -1,37 +1,20 @@
 #include <iostream>
 using namespace std;
 
+bool check_8(int x) {
+	while (x != 0) 
+		if (x % 10 != 8)
+			x = int(x / 10) ;
+		else
+			return 1; 
+	return 0;
+}
+
 int floors(int x) {
-	int tmp = x % 10;
-	if (x >= 0)
-		if (tmp < 8) 
-			return 8 - tmp;
-		else {
-			x = int(x / 10);
-			while (x % 10 == 9)
-				x = int(x / 10);
-			if (x % 10 == 7)
-				return 10 - tmp;
-			else 
-				return 18 - tmp;
-		}
-	else {
-		x = -x;
-		if (x < 9)
-			return x + 8;
-		else if (x % 10 == 9)
-			return 1;
-		else {
-			tmp = x % 10;
-			x = int(x / 10);
-			while ((x % 10 == 0) && (int(x / 10) != 0))
-				x = int(x / 10);
-			if (x % 10 == 9)
-				return tmp + 1;
-			else 
-				return tmp + 2;
-		}
-	}
+	int tmp = 1;
+	while (check_8(x + tmp) == 0)
+		tmp += 1;
+	return tmp;
 }
 
 int main() {
