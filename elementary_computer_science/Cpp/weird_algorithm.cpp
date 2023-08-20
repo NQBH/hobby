9b62b9c43d36484ed53a1976fdffb63dd589108c
NQBH <nguyenquanbahong@gmail.com>
2023-08-20 20:23:25 +0700

    C++: CSES/weird algorithm

--- elementary_computer_science/Cpp/weird_algorithm.cpp
+++ elementary_computer_science/Cpp/weird_algorithm.cpp
@@ -5,10 +5,10 @@ int main() {
 	long long n;
 	cin >> n;
 	while (1) {
-		cout << n << " ";
+		cout << n << ' ';
 		if (n == 1) break;
-		if (n%2 == 0) n /= 2;
+		if (n % 2 == 0) n /= 2;
 		else n = n*3 + 1;
 	}
-	cout << "\n";
+	cout << '\n';
 }
